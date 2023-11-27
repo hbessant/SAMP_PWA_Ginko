aa1d472f539daf8147acf7d2b208e50b48c853ff
hbessant <bessant.hugo@gmail.com>
2023-11-27 11:28:52 +0100

    Ajout service Worker

---
+++ ginkobus-pwa/service-worker.js
@@ -0,0 +1,25 @@
+const cacheName = "ginkobus-pwa-v1"
+
+const contentToCache = [
+    "icons/icon-192.png",
+    "icons/icon-512.png",
+    "icons/maskable_icon.png",
+    "manifest.json",
+    "app.js",
+    "index.html",
+    "index.js",
+    "style.css"
+];
+
+
+
+self.addEventListener("install", (e) => {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+      (async () => {
+        const cache = await caches.open(cacheName);
+        console.log("[Service Worker] Caching all: app shell and content");
+        await cache.addAll(contentToCache);
+      })(),
+    );
+  });
